b7e5a43e8410be0caf9349402a727bea8db7d265
arumrhm <arum.120140085@student.itera.ac.id>
2023-03-29 22:08:01 +0700

    first commit

---
+++ qgis2web_2023_03_28-17_10_21_902727/styles/Mall_2_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_Mall_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#1fa8b4";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Nama") !== null) {
+        labelText = String(feature.get("Nama"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.03793103448275862,
+                  anchor: [11, 11],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/shopping_supermarket.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
